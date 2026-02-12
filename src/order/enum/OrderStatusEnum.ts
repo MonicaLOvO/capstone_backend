ae68e1f144bf5bed5e4c79f928c6bdb878ce66ae
Abdullah <abdullahdauda55@gmail.com>
2026-02-12 05:23:17 -0700

    Fix order enums

--- src/order/enum/OrderStatusEnum.ts
+++ src/order/enum/OrderStatusEnum.ts
@@ -1,9 +1,4 @@
 export enum OrderStatusEnum {
-    Pending = "0",
-    Processing = "1",
-    Shipped = "2",
-    Delivered = "3",
-    Cancelled = "4",
-    Returned = "5",
-    Completed = "6",
+    Processing = "0",
+    Pending = "1"
 }
